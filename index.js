aa323bef8948657eb0c7a6dba984073d7e716458
jpsinieghi <jpsinieghi@gmail.com>
2020-06-22 14:49:28 -0300

    final02

--- index.js
+++ index.js
@@ -1,68 +1,51 @@
-//https://github.com/akhil-kn/node-m3u8-to-mp3
 const ffmpeg = require('fluent-ffmpeg');
 const { exec } = require('child_process');
 var schedule = require('node-schedule');
-
-
 var rule = new schedule.RecurrenceRule();
 
 rule.minute = new schedule.Range(0, 59, 60);
 
 
 schedule.scheduleJob(rule, function(){
-  console.log('Rodando Script a cada 1 hora');
+    console.log('Rodando Script a cada 1 hora');
+    var today = new Date()
+    var min = String(today.getMinutes())
+    var hour = String(today.getHours())
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = dd+mm+yyyy+'-'+hour+'_'+min;
 
+    const config = {
+        "cover": "./assets/radiocn.jpg",
+        "name": today,
+        "m3u8_path": "https://rdamchp1-lh.akamaihd.net/i/rdamchp_1_0@193678/master.m3u8",
+        "bitrate": "96"
+    };
 
-var today = new Date()
-var min = String(today.getMinutes())
-var hour = String(today.getHours())
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-today = dd+mm+yyyy+'-'+hour+'_'+min;
+    const file_path = `Music/${config.name}.mp3`;
+	const stream = ffmpeg(config.m3u8_path)
+        .on('start', () => { console.log('Processing started !'); })
+        .on('progress', (progress) => {console.log('Timemark: ' + progress.timemark);})
+        .on('end', () => { addCover(); })
+        .on('error', (err) => { console.log('An error occurred: ' + err.message); })
+        .audioCodec('libmp3lame')
+        .audioBitrate(config.bitrate)
+        .mergeToFile(config.name+'_temp.mp3', './Music')
+   
+    const stop = (audio) => {
+        return audio.ffmpegProc.stdin.write('q');
+    }
 
-
-
-const config = {
-    "cover": "./assets/radiocn.jpg",
-    "name": today,
-    "m3u8_path": "https://rdamchp1-lh.akamaihd.net/i/rdamchp_1_0@193678/master.m3u8",
-    "bitrate": "96"
-};
-
-const file_path = `Music/${config.name}.mp3`;
-
-
-
-		const stream = ffmpeg(config.m3u8_path)
-        //.duration('1:00')
-    .on('start', () => { console.log('Processing started !'); })
-    .on('progress', (progress) => {console.log('Timemark: ' + progress.timemark);})
-    .on('end', () => { addCover(); })
-    .on('error', (err) => { console.log('An error occurred: ' + err.message); })
-    .audioCodec('libmp3lame')
-    .audioBitrate(config.bitrate)
-    .mergeToFile(config.name+'_temp.mp3', './Music')
-
-    
-const stop = (audio) => {
-   return audio.ffmpegProc.stdin.write('q');
-}
-
-
-function addCover() {
-    const cmd = `ffmpeg -i ${config.name}"_temp.mp3" -i ${config.cover} -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" \"${file_path}\" && del ${config.name}"_temp.mp3"`;
-    exec(cmd, () => {
+    function addCover() {
+        const cmd = `ffmpeg -i ${config.name}"_temp.mp3" -i ${config.cover} -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" \"${file_path}\" && del ${config.name}"_temp.mp3"`;
+        exec(cmd, () => {
         console.log(`Processing finished !\nfile path : ${encodeURI(file_path)}`);
-    });
-}
+        });
+    }
 
-//3580000
-setTimeout(() => {
-    // safely end the ffmpeg process without destroying the file.
-    
-    stop(stream)
-    
-  }, 3580000);
+    setTimeout(() => {
+        stop(stream)
+    }, 3580000); //(1000*60*60)-20000
   
 });
